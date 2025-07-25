6cf048d78aa0877e322ab79f53d8d0e0db56bca0
kazu-iroiro <139434644+kazu-iroiro@users.noreply.github.com>
2025-07-26 01:25:29 +0900

    更新

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 (window.onload = function () {
-    function makeQR() {
-        var unixtime = Math.round((new Date()).getTime() / 1000);
+    setInterval(() => {
+        setTime(nowTimeDisp, "Now", new Date());
+    }, 1000)
 
-        var qr = new QRious({
-            element: document.getElementById('qr'),
-            value: 'https://synctimer.iroirolink.com/index.html?'+ unixtime
-        });
-    }
-
-
-    let makeQrButton = document.getElementById("makeQrButton");
-
-    makeQrButton.addEventListener(
-        'click', makeQR()
-    );
+    setInterval(() => {
+        setTime(startTimeDisp, "Start", new Date());
+    }, 1000)
 })();
+
+function getdoubleDigestNumer(number) {
+  return ("0" + number).slice(-2)
+}
+
+var unixtime = Math.round((new Date()).getTime() / 1000);
+let qr = new QRious({
+    element: document.getElementById('qr'),
+    value: 'https://synctimer.iroirolink.com/index.html?' + unixtime
+});
+
+function makeQR() {
+    console.log("aaa");
+    unixtime = Math.round((new Date()).getTime() / 1000);
+    qr.value = 'https://synctimer.iroirolink.com/index.html?' + unixtime;
+}
+
+
+let nowTimeDisp = document.getElementById("nowTime");
+let startTimeDisp = document.getElementById("startTime");
+
+function setTime(object, str, datetime) {
+    object.innerText= str + ": " + String(getdoubleDigestNumer(datetime.getHours())) + ":" + String(getdoubleDigestNumer(datetime.getMinutes()) + ":" + String(getdoubleDigestNumer(datetime.getSeconds())));
+}
+
+
+let makeQrButton = document.getElementById("makeQR");
+makeQrButton.addEventListener('click',
+    function () {
+        makeQR()
+    }, false);
