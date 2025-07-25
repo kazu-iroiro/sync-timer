1ea8db9ed6fe4cbbd31332fe1507478818eaa571
kazu-iroiro <139434644+kazu-iroiro@users.noreply.github.com>
2025-07-26 03:12:43 +0900

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -5,20 +5,20 @@
 
         if (!startTimeFlag)
             startTime = nowTime;
-            setTime(startTimeDisp, "Start", startTime);
+        setTime(startTimeDisp, "Start", startTime);
 
         var diffMilliSec = (nowTime) - startTime;
 
-        var minus="+";
+        var minus = "+";
         if (diffMilliSec < 0)
             minus = "-"
 
         // 秒数の計算
-        var tmpSec = Math.abs(diffMilliSec / 1000);
+        var tmpSec = Math.abs((diffMilliSec - diffMilliSec % 1000) / 1000);
         // 秒として残る部分
         var Sec = Math.floor(tmpSec % 60);
         // 分として分けられる部分
-        var tmpMin = Math.floor(tmpSec / 60);
+        var tmpMin = tmpSec / 60;
         // 実際に分の部分
         var Min = Math.floor(tmpMin % 60);
         // 時間として残る部分
@@ -29,46 +29,89 @@
     }, 1000)
 })();
 
+
+var startTime;
+var startTimeFlag = false;
+
+const searchParams = new URLSearchParams(window.location.search)
+if(searchParams.has('unixStartTime')) {
+    console.log(searchParams.get('unixStartTime'));
+    startTime = new Date(searchParams.get('unixStartTime')*1000);
+    startTimeFlag = true;
+}
+else
+    startTime = new Date();
 function getdoubleDigestNumer(number) {
     return ("0" + number).slice(-2)
 }
 
-var startTime = new Date();
-
 let qr = new QRious({
     element: document.getElementById('qr'),
-    value: 'https://synctimer.iroirolink.com/index.html?'
+    value: 'https://synctimer.iroirolink.com/index.html?unixStartTime=' + Math.round((new Date()).getTime() / 1000)
 });
 
 function makeQR() {
     var unixStartTime = Math.round(startTime.getTime() / 1000);
-    unixtime = Math.round((new Date()).getTime() / 1000);
     qr.value = 'https://synctimer.iroirolink.com/index.html?unixStartTime=' + unixStartTime;
 }
 
-var startTimeFlag = false;
 let nowTimeDisp = document.getElementById("nowTime");
 let startTimeDisp = document.getElementById("startTime");
 let targetTimeDisp = document.getElementById("targetTime");
 
 function setTime(object, str, datetime) {
     object.innerText = str + ": " + String(getdoubleDigestNumer(datetime.getHours())) + ":" + String(getdoubleDigestNumer(datetime.getMinutes()) + ":" + String(getdoubleDigestNumer(datetime.getSeconds())));
+    if (str == "Start" && startTimeFlag)
+        makeQR();
 }
-
-
-let makeQrButton = document.getElementById("makeQR");
-makeQrButton.addEventListener('click',
-    function () {
-        makeQR()
-    }, false);
 
 document.getElementById("plusTenHours").addEventListener('click', function () {
     startTimeFlag = true; startTime.setHours(startTime.getHours() + 10); setTime(startTimeDisp, "Start", startTime);
-}, false)
+}, false);
 document.getElementById("plusTenMins").addEventListener('click', function () {
     startTimeFlag = true; startTime.setMinutes(startTime.getMinutes() + 10); setTime(startTimeDisp, "Start", startTime);
-}, false)
+}, false);
 document.getElementById("plusTenSecs").addEventListener('click', function () {
     startTimeFlag = true; startTime.setSeconds(startTime.getSeconds() + 10); setTime(startTimeDisp, "Start", startTime);
-}, false)
+}, false);
 
+document.getElementById("plusFiveHours").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setHours(startTime.getHours() + 5); setTime(startTimeDisp, "Start", startTime);
+}, false);
+document.getElementById("plusFiveMins").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setMinutes(startTime.getMinutes() + 5); setTime(startTimeDisp, "Start", startTime);
+}, false);
+document.getElementById("plusFiveSecs").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setSeconds(startTime.getSeconds() + 5); setTime(startTimeDisp, "Start", startTime);
+}, false);
+
+document.getElementById("plusOneHour").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setHours(startTime.getHours() + 1); setTime(startTimeDisp, "Start", startTime);
+}, false);
+document.getElementById("plusOneMin").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setMinutes(startTime.getMinutes() + 1); setTime(startTimeDisp, "Start", startTime);
+}, false);
+document.getElementById("plusOneSec").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setSeconds(startTime.getSeconds() + 1); setTime(startTimeDisp, "Start", startTime);
+}, false);
+
+document.getElementById("startStopWatch").addEventListener('click', function () {
+    startTimeFlag = true; setTime(startTimeDisp, "Start", startTime);
+}, false);
+
+document.getElementById("shareButton").addEventListener('click', async function () {
+    if (!startTimeFlag)
+        alert("時刻を固定しろやください")
+    else
+        var link = 'https://synctimer.iroirolink.com/index.html?unixStartTime=' + Math.round(startTime.getTime() / 1000);
+    try {
+        await navigator.share(link);
+    } catch (err) {
+        console.log(err);
+        try {
+            await navigator.clipboard.writeText(link);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}, false);
