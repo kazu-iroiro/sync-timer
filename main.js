7fa551c385d45e67f4d922a449d5dec7ec52e885
kazu-iroiro <139434644+kazu-iroiro@users.noreply.github.com>
2025-07-26 01:59:31 +0900

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -4,32 +4,39 @@
     }, 1000)
 
     setInterval(() => {
-        setTime(startTimeDisp, "Start", new Date());
+        if (!startTimeFlag)
+            setTime(startTimeDisp, "Start", new Date());
+    }, 1000)
+
+    setInterval(() => {
+        setTime(targetTimeDisp, "Diff", (new Date()-startTimeDisp));
     }, 1000)
 })();
 
 function getdoubleDigestNumer(number) {
-  return ("0" + number).slice(-2)
+    return ("0" + number).slice(-2)
 }
 
-var unixtime = Math.round((new Date()).getTime() / 1000);
+var startTime = new Date();
+
 let qr = new QRious({
     element: document.getElementById('qr'),
-    value: 'https://synctimer.iroirolink.com/index.html?' + unixtime
+    value: 'https://synctimer.iroirolink.com/index.html?'
 });
 
 function makeQR() {
-    console.log("aaa");
+    var unixStartTime = Math.round(startTime.getTime() / 1000);
     unixtime = Math.round((new Date()).getTime() / 1000);
-    qr.value = 'https://synctimer.iroirolink.com/index.html?' + unixtime;
+    qr.value = 'https://synctimer.iroirolink.com/index.html?unixStartTime=' + unixStartTime;
 }
 
-
+var startTimeFlag = false;
 let nowTimeDisp = document.getElementById("nowTime");
 let startTimeDisp = document.getElementById("startTime");
+let targetTimeDisp = document.getElementById("targetTime");
 
 function setTime(object, str, datetime) {
-    object.innerText= str + ": " + String(getdoubleDigestNumer(datetime.getHours())) + ":" + String(getdoubleDigestNumer(datetime.getMinutes()) + ":" + String(getdoubleDigestNumer(datetime.getSeconds())));
+    object.innerText = str + ": " + String(getdoubleDigestNumer(datetime.getHours())) + ":" + String(getdoubleDigestNumer(datetime.getMinutes()) + ":" + String(getdoubleDigestNumer(datetime.getSeconds())));
 }
 
 
@@ -38,3 +45,14 @@ makeQrButton.addEventListener('click',
     function () {
         makeQR()
     }, false);
+
+document.getElementById("plusTenHours").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setHours(startTime.getHours() + 10); setTime(startTimeDisp, "Start", startTime);
+}, false)
+document.getElementById("plusTenMins").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setMinutes(startTime.getMinutes() + 10); setTime(startTimeDisp, "Start", startTime);
+}, false)
+document.getElementById("plusTenSecs").addEventListener('click', function () {
+    startTimeFlag = true; startTime.setSeconds(startTime.getSeconds() + 10); setTime(startTimeDisp, "Start", startTime);
+}, false)
+
