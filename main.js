6b9f28d0edd24bd7e402b50696307fb327920693
kazu_iroiro <139434644+kazu-iroiro@users.noreply.github.com>
2025-07-28 19:32:29 +0900

    Update main.js
    前回の修正をロールバック

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
         setTime(nowTimeDisp, "Now", nowTime);
 
         if (!startTimeFlag)
-            startTime = nowTime - nowTime % 1000;
+            startTime = nowTime;
         setTime(startTimeDisp, "Start", startTime);
 
         var diffMilliSec = (nowTime) - startTime;
